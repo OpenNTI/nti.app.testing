4d5d3ec277b33bba47da7202d2e6b06a7e2173b7
Jason Madden <jason.madden@nextthought.com>
2013-12-11 14:00:41 +0000

    Round one of deprecation fixup: authenticated_userid.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.testing/trunk@27970 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/testing/matchers.py
+++ src/nti/app/testing/matchers.py
@@ -15,8 +15,6 @@ logger = __import__('logging').getLogger(__name__)
 
 from nti.testing.matchers import BoolMatcher
 
-from pyramid.security import has_permission as _p_has_permission
-
 class HasPermission(BoolMatcher):
 
 	def __init__( self, value, permission, request ):
@@ -26,7 +24,7 @@ class HasPermission(BoolMatcher):
 
 	def _matches(self, item):
 		return super(HasPermission,self)._matches(
-			_p_has_permission( self.permission, item, self.request ) )
+			self.request.has_permission( self.permission, item ) )
 
 def has_permission( permission, request ):
 	return HasPermission(True, permission, request)
