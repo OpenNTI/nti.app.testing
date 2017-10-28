62d98debcc1e7376c5b2b8150db3ad39e8767d2d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-28 03:22:40 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.testing/trunk@123570 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/testing/tests/test_empty.py
+++ src/nti/app/testing/tests/test_empty.py
@@ -8,10 +8,11 @@ from __future__ import absolute_import
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904,W0611
 
-import nti.app.testing
-import nti.app.testing.base
-import nti.app.testing.matchers
-import nti.app.testing.request_response
+__import__('nti.app.testing')
+__import__('nti.app.testing.base')
+__import__('nti.app.testing.matchers')
+__import__('nti.app.testing.request_response')
+
 
 def test_empty():
-	return
+    return
