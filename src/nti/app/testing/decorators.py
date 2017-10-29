fb5af1d18b76be35b699b44775f52e310ae514c8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-29 00:21:41 +0000

    pylint;pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.testing/trunk@123579 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/testing/decorators.py
+++ src/nti/app/testing/decorators.py
@@ -4,10 +4,9 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 # disable: accessing protected members, too many methods
 # pylint: disable=I0011,W0212,R0904
@@ -15,128 +14,137 @@ logger = __import__('logging').getLogger(__name__)
 import warnings
 import functools
 
+from nti.app.testing.webtest import TestApp
+
 from nti.dataserver.tests import mock_dataserver
 
-from .webtest import TestApp
+logger = __import__('logging').getLogger(__name__)
 
-def WithSharedApplicationMockDS( *args, **kwargs ):
-	"""
-	Decorator for a test function using the shared application.
-	Unknown keyword arguments are passed to :func:`.WithMockDS`.
 
-	:keyword users: Either `True`, or a sequence of strings naming users. If True,
-		then the standard user is created. If a sequence, then the standard user,
-		followed by each named user is created. This is done by calling a method
-		``_create_user`` on ``self``.
-	:keyword bool default_authenticate: Only used if ``users`` was a sequence
-		(and so we have created at least two users). If set to `True` (NOT the default),
-		then ``self.testapp`` will be authenticated by the standard user.
-	:keyword bool testapp: If True (NOT the default) then ``self.testapp`` will
-		be created.
-	:keyword function users_hook: If given, a function that will be called with
-		a mapping {username:user} after we have created all users, in the scope
-		of the transaction.
-	"""
+def WithSharedApplicationMockDS(*args, **kwargs):
+    """
+    Decorator for a test function using the shared application.
+    Unknown keyword arguments are passed to :func:`.WithMockDS`.
 
-	users_to_create = kwargs.pop( 'users', None )
-	if isinstance(users_to_create,basestring):
-		# They forgot the comma to make a tuple
-		users_to_create = (users_to_create,)
-	default_authenticate = kwargs.pop( 'default_authenticate', None )
-	testapp = kwargs.pop( 'testapp', None )
-	if 'handle_changes' in kwargs:
-		val = kwargs.pop( 'handle_changes', False )
-		warnings.warn('handle_changes is always on', DeprecationWarning, stacklevel=1)
-		kwargs['with_changes'] =  val # needed to get the decorator vs factory logic correct below
-	user_hook = kwargs.pop( 'user_hook', None )
-	users_hook = kwargs.pop( 'users_hook', None )
+    :keyword users: Either `True`, or a sequence of strings naming users. If True,
+            then the standard user is created. If a sequence, then the standard user,
+            followed by each named user is created. This is done by calling a method
+            ``_create_user`` on ``self``.
+    :keyword bool default_authenticate: Only used if ``users`` was a sequence
+            (and so we have created at least two users). If set to `True` (NOT the default),
+            then ``self.testapp`` will be authenticated by the standard user.
+    :keyword bool testapp: If True (NOT the default) then ``self.testapp`` will
+            be created.
+    :keyword function users_hook: If given, a function that will be called with
+            a mapping {username:user} after we have created all users, in the scope
+            of the transaction.
+    """
 
-	if testapp:
-		def _make_app(self):
-			if ((users_to_create is True and default_authenticate is not False)
-				or (users_to_create and default_authenticate)):
-				self.testapp = TestApp( self.app, extra_environ=self._make_extra_environ() )
-			else:
-				self.testapp = TestApp( self.app )
+    users_to_create = kwargs.pop('users', None)
+    if isinstance(users_to_create, basestring):
+        # They forgot the comma to make a tuple
+        users_to_create = (users_to_create,)
+    default_authenticate = kwargs.pop('default_authenticate', None)
+    testapp = kwargs.pop('testapp', None)
+    if 'handle_changes' in kwargs:
+        val = kwargs.pop('handle_changes', False)
+        warnings.warn('handle_changes is always on',
+                      DeprecationWarning, stacklevel=1)
+        # needed to get the decorator vs factory logic correct below
+        kwargs['with_changes'] = val
+    user_hook = kwargs.pop('user_hook', None)
+    users_hook = kwargs.pop('users_hook', None)
 
-	else:
-		def _make_app( self ):
-			pass
+    if testapp:
+        def _make_app(self):
+            if (   (users_to_create is True and default_authenticate is not False)
+                or (users_to_create and default_authenticate)):
+                self.testapp = TestApp(self.app, 
+                                       extra_environ=self._make_extra_environ())
+            else:
+                self.testapp = TestApp(self.app)
 
-	if users_to_create:
-		def _do_create(self):
-			with mock_dataserver.mock_db_trans( self.ds ):
-				base_user = self._create_user()
-				self.users = { base_user.username: base_user }
-				if user_hook:
-					user_hook( base_user )
-				if users_to_create and users_to_create is not True:
-					for username in users_to_create:
-						self.users[username] = self._create_user( username )
-				if users_hook:
-					users_hook(self.users)
-	else:
-		def _do_create(self):
-			pass
+    else:
+        def _make_app(self):
+            pass
 
-	if len(args) == 1 and not kwargs:
-		# being used as a decorator:
-		# @WithSharedApplicatonMockDS
-		# def test_foo(...):
+    if users_to_create:
+        def _do_create(self):
+            with mock_dataserver.mock_db_trans(self.ds):
+                base_user = self._create_user()
+                self.users = {base_user.username: base_user}
+                if user_hook:
+                    user_hook(base_user)
+                if users_to_create and users_to_create is not True:
+                    for username in users_to_create:
+                        self.users[username] = self._create_user(username)
+                if users_hook:
+                    users_hook(self.users)
+    else:
+        def _do_create(self):
+            pass
 
-		kwargs = {'base_storage': lambda self: self._storage_base}
-		func = args[0]
+    if len(args) == 1 and not kwargs:
+        # being used as a decorator:
+        # @WithSharedApplicatonMockDS
+        # def test_foo(...):
 
-		@functools.wraps(func)
-		@mock_dataserver.WithMockDS(**kwargs)
-		def f(self):
-			self.config.registry._zodb_databases = { '': self.ds.db } # 0.3
-			self.ds.redis.flushall()
-			_do_create( self )
-			_make_app( self )
-			func(self)
+        kwargs = {'base_storage': lambda self: self._storage_base}
+        func = args[0]
 
-		return f
+        @functools.wraps(func)
+        @mock_dataserver.WithMockDS(**kwargs)
+        def f(self):
+            self.config.registry._zodb_databases = {'': self.ds.db}  # 0.3
+            self.ds.redis.flushall()
+            _do_create(self)
+            _make_app(self)
+            func(self)
 
-	# Being used as a decorator factory:
-	# @WithSharedApplicationMockDS(...)
-	# def test_foo(...):
+        return f
 
-	# Are we in a layer that set up shared storage for us?
-	kwargs['base_storage'] = lambda self: getattr(self, '_storage_base', None)
-	def factory(func):
-		@functools.wraps(func)
-		@mock_dataserver.WithMockDS(**kwargs)
-		def f(self):
-			self.config.registry._zodb_databases = { '': self.ds.db } # 0.3
-			self.ds.redis.flushall()
-			_do_create( self )
-			_make_app( self )
-			if getattr(self, 'setUpDs', None):
-				self.setUpDs(self.ds)
-			func(self)
-		return f
-	return factory
+    # Being used as a decorator factory:
+    # @WithSharedApplicationMockDS(...)
+    # def test_foo(...):
 
-def WithSharedApplicationMockDSHandleChanges( *args, **kwargs ):
-	call_factory = False
-	if len(args) == 1 and not kwargs:
-		# Being used as a plain decorator. But we add kwargs that make
-		# it look like we're being used as a factory
-		call_factory = True
+    # Are we in a layer that set up shared storage for us?
+    kwargs['base_storage'] = lambda self: getattr(self, '_storage_base', None)
 
-	kwargs['handle_changes'] = True # yes, deprecated, but necessary to ensure kwargs not empty
-	if 'testapp' not in kwargs:
-		kwargs['testapp'] = True
-	result = WithSharedApplicationMockDS( *args, **kwargs )
-	if call_factory:
-		result = result(args[0])
-	return result
+    def factory(func):
+        @functools.wraps(func)
+        @mock_dataserver.WithMockDS(**kwargs)
+        def f(self):
+            self.config.registry._zodb_databases = {'': self.ds.db}  # 0.3
+            self.ds.redis.flushall()
+            _do_create(self)
+            _make_app(self)
+            if getattr(self, 'setUpDs', None):
+                self.setUpDs(self.ds)
+            func(self)
+        return f
+    return factory
+
+
+def WithSharedApplicationMockDSHandleChanges(*args, **kwargs):
+    call_factory = False
+    if len(args) == 1 and not kwargs:
+        # Being used as a plain decorator. But we add kwargs that make
+        # it look like we're being used as a factory
+        call_factory = True
+    # yes, deprecated, but necessary to ensure kwargs not empty
+    kwargs['handle_changes'] = True
+    if 'testapp' not in kwargs:
+        kwargs['testapp'] = True
+    result = WithSharedApplicationMockDS(*args, **kwargs)
+    if call_factory:
+        result = result(args[0])
+    return result
+
 
 def WithSharedApplicationMockDSWithChanges(func):
-	@functools.wraps(func)
-	@mock_dataserver.WithMockDS(with_changes=True)
-	def f(self):
-		self.config.registry._zodb_databases = { '': self.ds.db } # 0.3
-		func(self)
-	return f
+    @functools.wraps(func)
+    @mock_dataserver.WithMockDS(with_changes=True)
+    def f(self):
+        self.config.registry._zodb_databases = {'': self.ds.db}  # 0.3
+        func(self)
+    return f
--- src/nti/app/testing/layers.py
+++ src/nti/app/testing/layers.py
@@ -6,10 +6,9 @@ Application test layers.
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from nti.monkey import patch_pyramid_on_import
 patch_pyramid_on_import.patch()
@@ -19,16 +18,26 @@ patch_pyramid_on_import.patch()
 
 __test__ = False
 
-import pyramid
+from pyramid.interfaces import IRendererFactory
+from pyramid.interfaces import IAuthorizationPolicy
+from pyramid.interfaces import IAuthenticationPolicy
+
 from pyramid.testing import setUp as psetUp
 from pyramid.testing import tearDown as ptearDown
 
 import ZODB.DemoStorage
 
-from zope import component
 import zope.testing.cleanup
 
+from zope import component
+
 from nti.app.pyramid_zope import z3c_zpt
+
+from nti.app.testing.base import DummyRequest
+from nti.app.testing.base import _PWManagerMixin
+
+from nti.app.testing.testing import TestMailDelivery
+from nti.app.testing.testing import ITestMailDelivery
 
 from nti.dataserver.tests.mock_dataserver import DSInjectorMixin
 
@@ -36,228 +45,240 @@ from nti.testing.layers import find_test
 from nti.testing.layers import ZopeComponentLayer
 from nti.testing.layers import ConfiguringLayerMixin
 
-from .base import DummyRequest
-from .base import _PWManagerMixin
+logger = __import__('logging').getLogger(__name__)
 
-from .testing import TestMailDelivery
-from .testing import ITestMailDelivery
 
 class PyramidLayerMixin(object):
 
+    _mailer = None
 
-	_mailer = None
+    security_policy = None
 
-	security_policy = None
+    _pwman = None
 
-	_pwman = None
+    #: A demo storage will be created on top of this
+    #: storage. This can be used to create objects at
+    #: class or module set up time and have them available
+    #: to dataservers created at test method set up time.
+    _storage_base = None
 
-	#: A demo storage will be created on top of this
-	#: storage. This can be used to create objects at
-	#: class or module set up time and have them available
-	#: to dataservers created at test method set up time.
-	_storage_base = None
+    request = None
+    set_up_mailer = True
 
-	request = None
-	set_up_mailer = True
+    @classmethod
+    def setUpPyramid(cls,
+                     request_factory=DummyRequest,
+                     request_args=(),
+                     security_policy_factory=None,
+                     force_security_policy=True):
+        """
+        :return: The `Configurator`, which is also in ``self.config``.
+        """
+        __traceback_info__ = request_factory, request_args
+        cls._pwman = _PWManagerMixin()
+        cls._pwman.setUpPasswords()
 
-	@classmethod
-	def setUpPyramid(cls,
-					 request_factory=DummyRequest,
-					 request_args=(),
-					 security_policy_factory=None,
-					 force_security_policy=True ):
-		"""
-		:return: The `Configurator`, which is also in ``self.config``.
-		"""
-		cls._pwman = _PWManagerMixin()
-		cls._pwman.setUpPasswords()
+        # The demo storage has less strict requirements about
+        # being open/closed than a plain mapping storage
+        cls._storage_base = ZODB.DemoStorage.DemoStorage()
 
-		# The demo storage has less strict requirements about
-		# being open/closed than a plain mapping storage
-		cls._storage_base = ZODB.DemoStorage.DemoStorage()
+        cls.config = psetUp(registry=component.getGlobalSiteManager(
+        ), request=cls.request, hook_zca=False)
+        cls.config.setup_registry()
 
-		cls.config = psetUp(registry=component.getGlobalSiteManager(),request=cls.request,hook_zca=False)
-		cls.config.setup_registry()
+        if cls.set_up_mailer:
+            # Must provide the correct zpt template renderer or the email process blows up
+            # See application.py
+            cls.config.include('pyramid_chameleon')
+            cls.config.include('pyramid_mako')
+            component.provideUtility(z3c_zpt.renderer_factory,
+                                     IRendererFactory,
+                                     name=".pt")
+            cls._mailer = mailer = TestMailDelivery()
+            component.provideUtility(mailer, ITestMailDelivery)
 
-		if cls.set_up_mailer:
-			# Must provide the correct zpt template renderer or the email process blows up
-			# See application.py
-			cls.config.include('pyramid_chameleon')
-			cls.config.include('pyramid_mako')
-			component.provideUtility( z3c_zpt.renderer_factory, pyramid.interfaces.IRendererFactory, name=".pt" )
-			cls._mailer = mailer = TestMailDelivery()
-			component.provideUtility( mailer, ITestMailDelivery )
+        if security_policy_factory:
+            cls.security_policy = security_policy_factory()
+            for iface in IAuthenticationPolicy, IAuthorizationPolicy:
+                if iface.providedBy(cls.security_policy) or force_security_policy:
+                    component.provideUtility(cls.security_policy, iface)
+        return cls.config
 
+    @classmethod
+    def tearDownPyramid(cls):
+        ptearDown()
+        cls._mailer = None
+        cls.security_policy = None
+        cls._pwman.tearDownPasswords()
+        cls._pwman = None
+        zope.testing.cleanup.cleanUp()
+        setHooks()  # but these must be back!
 
-		if security_policy_factory:
-			cls.security_policy = security_policy_factory()
-			for iface in pyramid.interfaces.IAuthenticationPolicy, pyramid.interfaces.IAuthorizationPolicy:
-				if iface.providedBy( cls.security_policy ) or force_security_policy:
-					component.provideUtility( cls.security_policy, iface )
-		return cls.config
+    @classmethod
+    def testSetUpPyramid(cls, test=None):
+        test = test or find_test()
+        test.config = cls.config
+        if cls._mailer:
+            del cls._mailer.queue[:]
 
-	@classmethod
-	def tearDownPyramid( cls ):
-		ptearDown()
-		cls._mailer = None
-		cls.security_policy = None
-		cls._pwman.tearDownPasswords()
-		cls._pwman = None
-		zope.testing.cleanup.cleanUp()
-		setHooks() # but these must be back!
+    @classmethod
+    def testTearDownPyramid(cls):
+        try:
+            del find_test().config
+        except AttributeError:
+            pass
 
-	@classmethod
-	def testSetUpPyramid( cls, test=None ):
-		test = test or find_test()
-		test.config = cls.config
-		if cls._mailer:
-			del cls._mailer.queue[:]
+    @classmethod
+    def setUp(cls):
+        pass
 
-	@classmethod
-	def testTearDownPyramid(cls):
-		test = find_test()
-		del test.config
+    @classmethod
+    def tearDown(cls):
+        pass
 
-	@classmethod
-	def setUp(cls):
-		pass
+    @classmethod
+    def testSetUp(cls):
+        pass
 
-	@classmethod
-	def tearDown(cls):
-		pass
+    @classmethod
+    def testTearDown(cls):
+        # Must implement
+        pass
 
-	@classmethod
-	def testSetUp( cls ):
-		pass
-
-	@classmethod
-	def testTearDown(cls):
-		# Must implement
-		pass
 
 from zope.component.hooks import setHooks
 
+
 class AppTestLayer(ZopeComponentLayer,
-				   PyramidLayerMixin,
-				   ConfiguringLayerMixin,
-				   DSInjectorMixin):
-	set_up_packages = ('nti.appserver',)
-	@classmethod
-	def setUp(cls):
-		setHooks()
-		try:
-			cls.setUpPyramid()
-			cls.setUpPackages()
-		except:
-			print("WARNING: failed to set up layer", cls, "; cleaning up")
-			cls.tearDown()
-			raise
+                   PyramidLayerMixin,
+                   ConfiguringLayerMixin,
+                   DSInjectorMixin):
 
-	@classmethod
-	def tearDown(cls):
-		cls.tearDownPackages()
-		cls.tearDownPyramid()
-		zope.testing.cleanup.cleanUp()
-		setHooks() # but these must be back!
+    set_up_packages = ('nti.appserver',)
 
-	@classmethod
-	def testSetUp(cls, test=None):
-		cls.setUpTestDS(test)
-		cls.testSetUpPyramid(test)
+    @classmethod
+    def setUp(cls):
+        setHooks()
+        try:
+            cls.setUpPyramid()
+            cls.setUpPackages()
+        except:
+            print("WARNING: failed to set up layer", cls, "; cleaning up")
+            cls.tearDown()
+            raise
 
-	@classmethod
-	def testTearDown(cls):
-		cls.testTearDownPyramid()
+    @classmethod
+    def tearDown(cls):
+        cls.tearDownPackages()
+        cls.tearDownPyramid()
+        zope.testing.cleanup.cleanUp()
+        setHooks()  # but these must be back!
+
+    @classmethod
+    def testSetUp(cls, test=None):
+        cls.setUpTestDS(test)
+        cls.testSetUpPyramid(test)
+
+    @classmethod
+    def testTearDown(cls):
+        cls.testTearDownPyramid()
 
 
 class NewRequestAppTestLayer(AppTestLayer):
 
-	@classmethod
-	def setUp(cls):
-		# You MUST implement this, otherwise zope.testrunner
-		# will call the super-class again
-		setHooks()
+    @classmethod
+    def setUp(cls):
+        # You MUST implement this, otherwise zope.testrunner
+        # will call the super-class again
+        setHooks()
 
-	@classmethod
-	def tearDown(cls):
-		# You MUST implement this, otherwise zope.testrunner
-		# will call the super-class again
-		zope.testing.cleanup.cleanUp()
-		setHooks() # but these must be back!
+    @classmethod
+    def tearDown(cls):
+        # You MUST implement this, otherwise zope.testrunner
+        # will call the super-class again
+        zope.testing.cleanup.cleanUp()
+        setHooks()  # but these must be back!
 
-	@classmethod
-	def testSetUp( cls, test=None ):
-		test = test or find_test()
-		cls.testSetUpPyramid(test)
-		test.beginRequest()
+    @classmethod
+    def testSetUp(cls, test=None):
+        test = test or find_test()
+        cls.testSetUpPyramid(test)
+        test.beginRequest()
 
-	@classmethod
-	def testTearDown(cls):
-		# Must implement
-		pass
+    @classmethod
+    def testTearDown(cls):
+        # Must implement
+        pass
+
 
 SharedConfiguringTestLayer = AppTestLayer
 NewRequestSharedConfiguringTestLayer = NewRequestAppTestLayer
 
 import unittest
 
-from .base import TestBaseMixin
+from nti.app.testing.base import TestBaseMixin
 
-class AppLayerTest(TestBaseMixin,unittest.TestCase):
-	layer = AppTestLayer
 
-class NewRequestLayerTest(TestBaseMixin,unittest.TestCase):
-	layer = NewRequestAppTestLayer
+class AppLayerTest(TestBaseMixin, unittest.TestCase):
+    layer = AppTestLayer
+
+
+class NewRequestLayerTest(TestBaseMixin, unittest.TestCase):
+    layer = NewRequestAppTestLayer
+
 
 class NonDevmodeSharedConfiguringTestLayer(ZopeComponentLayer,
-										   PyramidLayerMixin,
-										   ConfiguringLayerMixin,
-										   DSInjectorMixin):
+                                           PyramidLayerMixin,
+                                           ConfiguringLayerMixin,
+                                           DSInjectorMixin):
 
-	features = ()
-	set_up_packages = ('nti.appserver',)
-	@classmethod
-	def setUp(cls):
-		setHooks()
-		cls.setUpPyramid()
-		cls.setUpPackages()
+    features = ()
+    set_up_packages = ('nti.appserver',)
 
-	@classmethod
-	def tearDown(cls):
-		cls.tearDownPackages()
-		cls.tearDownPyramid()
-		zope.testing.cleanup.cleanUp()
-		setHooks()
+    @classmethod
+    def setUp(cls):
+        setHooks()
+        cls.setUpPyramid()
+        cls.setUpPackages()
 
-	@classmethod
-	def testSetUp(cls, test=None):
-		cls.setUpTestDS(test)
-		cls.testSetUpPyramid(test)
+    @classmethod
+    def tearDown(cls):
+        cls.tearDownPackages()
+        cls.tearDownPyramid()
+        zope.testing.cleanup.cleanUp()
+        setHooks()
+
+    @classmethod
+    def testSetUp(cls, test=None):
+        cls.setUpTestDS(test)
+        cls.testSetUpPyramid(test)
+
 
 class NonDevmodeNewRequestSharedConfiguringTestLayer(NonDevmodeSharedConfiguringTestLayer):
 
-	@classmethod
-	def setUp(cls):
-		# You MUST implement this, otherwise zope.testrunner
-		# will call the super-class again
-		setHooks()
+    @classmethod
+    def setUp(cls):
+        # You MUST implement this, otherwise zope.testrunner
+        # will call the super-class again
+        setHooks()
 
-	@classmethod
-	def tearDown(cls):
-		# You MUST implement this, otherwise zope.testrunner
-		# will call the super-class again
-		zope.testing.cleanup.cleanUp()
-		setHooks()
+    @classmethod
+    def tearDown(cls):
+        # You MUST implement this, otherwise zope.testrunner
+        # will call the super-class again
+        zope.testing.cleanup.cleanUp()
+        setHooks()
 
-	@classmethod
-	def testSetUp( cls, test=None ):
-		test = test or find_test()
-		cls.testSetUpPyramid(test)
-		test.beginRequest()
+    @classmethod
+    def testSetUp(cls, test=None):
+        test = test or find_test()
+        cls.testSetUpPyramid(test)
+        test.beginRequest()
 
-	@classmethod
-	def testTearDown(cls):
-		pass
+    @classmethod
+    def testTearDown(cls):
+        pass
 
-class NonDevmodeNewRequestLayerTest(TestBaseMixin,unittest.TestCase):
-	layer = NonDevmodeNewRequestSharedConfiguringTestLayer
+
+class NonDevmodeNewRequestLayerTest(TestBaseMixin, unittest.TestCase):
+    layer = NonDevmodeNewRequestSharedConfiguringTestLayer
