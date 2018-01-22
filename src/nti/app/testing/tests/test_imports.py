5d5d35c988c9a3bb0af158d71cebe63a93e725eb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-01-22 19:02:03 +0000

    pylint
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.testing/trunk@125663 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/testing/tests/test_imports.py
+++ src/nti/app/testing/tests/test_imports.py
@@ -5,11 +5,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-# disable: accessing protected members, too many methods
-# pylint: disable=W0212,R0904,W0611
+# pylint: disable=protected-access,too-many-public-methods
 
-import importlib
 import unittest
+import importlib
+
 
 def _make_import_test(mod_name=None, root='nti.app.testing'):
     def test(self):
@@ -18,8 +18,8 @@ def _make_import_test(mod_name=None, root='nti.app.testing'):
             parts.append(mod_name)
         mod = importlib.import_module('.'.join(parts))
         self.assertIsNotNone(mod)
-
     return test
+
 
 class TestModuleImportSideEffects(unittest.TestCase):
     """
@@ -28,13 +28,11 @@ class TestModuleImportSideEffects(unittest.TestCase):
     """
 
     for mod_name in (None, 'base', 'matchers', 'request_response'):
-        test_name = 'test_'+ (mod_name if mod_name else 'root')
+        test_name = 'test_' + (mod_name if mod_name else 'root')
         test = _make_import_test(mod_name)
-        test.__name__ =  test_name
+        test.__name__ = test_name
         locals()[test_name] = test
+
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
-
-
-
