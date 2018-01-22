85d1cca00c107e749a1f21abd294e0b46eb01086
Chris Utz <chris.utz@nextthought.com>
2018-01-22 18:28:48 +0000

    remove empty test in favor of a test case that does the importing.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.testing/trunk@125662 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/testing/tests/test_imports.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904,W0611
+
+import importlib
+import unittest
+
+def _make_import_test(mod_name=None, root='nti.app.testing'):
+    def test(self):
+        parts = [root]
+        if mod_name:
+            parts.append(mod_name)
+        mod = importlib.import_module('.'.join(parts))
+        self.assertIsNotNone(mod)
+
+    return test
+
+class TestModuleImportSideEffects(unittest.TestCase):
+    """
+    A TestCase defining tests that import a set of modules from this
+    package.
+    """
+
+    for mod_name in (None, 'base', 'matchers', 'request_response'):
+        test_name = 'test_'+ (mod_name if mod_name else 'root')
+        test = _make_import_test(mod_name)
+        test.__name__ =  test_name
+        locals()[test_name] = test
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
+
+
+
