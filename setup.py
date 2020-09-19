01729cdeff0fc7169a31f7862a35e8365bb4cd8a
cutz <christopher.m.utz@gmail.com>
2020-09-19 12:37:33 -0500

    need to depend on nti.dataserver[test]

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=[
         'setuptools',
         'nti.app.pyramid_zope',
-        'nti.dataserver',
+        'nti.dataserver[test]',
         'nti.ntiids',
         'nti.monkey',
         'nti.property',
